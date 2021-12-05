4a0c03e7dea8d77bcc94242e450478b66b016910
Anbu <15264938+anburocky3@users.noreply.github.com>
2021-12-06 02:36:22 +0530

    Added: Navbar
    + Using roboto font
    +  Using green theme colors

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template >
-  <div class="bg-indigo-500">Hello world</div>
+  <div class="min-h-screen bg-gray-100">
+    <div>
+      <TheNavbar />
+    </div>
+    <main class="my-10 mx-10">
+      <div class>Hello world</div>
+    </main>
+  </div>
 </template>
 
 <script setup>
+import TheNavbar from './components/commons/TheNavbar.vue';
 </script>
 
 <style>
---
+++ src/components/commons/TheNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-white shadow-md font-roboto text-green-500 px-10 py-5">
+        <div class="container mx-auto flex justify-between items-center">
+            <div>
+                <a href="#" class="flex space-x-4">
+                    <img src="../../assets/logo.png" alt="Vue Logo" class="w-8 h-8" />
+                    <h4 class="text-lg font-semibold">vBlog</h4>
+                </a>
+            </div>
+            <div>
+                <nav>
+                    <ul class="flex items-center space-x-8">
+                        <li>
+                            <a href="#" class="hover:text-green-600">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-green-600">Categories</a>
+                        </li>
+                        <li>
+                            <button
+                                class="px-4 py-2 rounded bg-green-500 text-white hover:bg-green-600"
+                            >Login / Register</button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
